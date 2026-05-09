2b29dd4bc7dd197dd4aa094540650f1a2d9f5555
Adrian Jackson <a.jackson@epcc.ed.ac.uk>
2026-05-09 15:10:18 +0100

    Fixing missing f08 symbols

--- src/mpi_communication_tracking_f08_helpers.c
+++ src/mpi_communication_tracking_f08_helpers.c
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 #include <mpi.h>
 #include <ISO_Fortran_binding.h>
 
@@ -53,6 +54,61 @@ void mpi_trace_f08_wait_helper(int *request_f, MPI_Status *status, int *ierr)
         *request_f = (int)PMPI_Request_c2f(c_req);
     }
 
+    *ierr = rc;
+}
+
+void mpi_trace_f08_isend_helper(CFI_cdesc_t *buf, int count, int datatype_f, int dest, int tag, int comm_f, int *request_f, int *ierr)
+{
+    const void *base = (buf != NULL) ? buf->base_addr : NULL;
+    MPI_Request c_req = MPI_REQUEST_NULL;
+    int rc;
+
+    if (ierr == NULL) return;
+
+    rc = (int)MPI_Isend(base, count, PMPI_Type_f2c((MPI_Fint)datatype_f), dest, tag, MPI_Comm_f2c((MPI_Fint)comm_f), &c_req);
+
+    if (request_f != NULL) {
+        *request_f = (int)PMPI_Request_c2f(c_req);
+    }
+
+    *ierr = rc;
+}
+
+void mpi_trace_f08_waitall_helper(int count, int request_f[], MPI_Status statuses[], int *ierr)
+{
+    int i;
+    MPI_Request *c_requests = NULL;
+    int rc;
+
+    if (ierr == NULL) return;
+
+    if (count < 0) {
+        *ierr = MPI_ERR_ARG;
+        return;
+    }
+
+    if (count == 0) {
+        *ierr = (int)MPI_Waitall(0, NULL, statuses);
+        return;
+    }
+
+    c_requests = (MPI_Request *)malloc((size_t)count * sizeof(MPI_Request));
+    if (c_requests == NULL) {
+        *ierr = MPI_ERR_NO_MEM;
+        return;
+    }
+
+    for (i = 0; i < count; i++) {
+        c_requests[i] = PMPI_Request_f2c((MPI_Fint)request_f[i]);
+    }
+
+    rc = (int)MPI_Waitall(count, c_requests, statuses);
+
+    for (i = 0; i < count; i++) {
+        request_f[i] = (int)PMPI_Request_c2f(c_requests[i]);
+    }
+
+    free(c_requests);
     *ierr = rc;
 }
 
--- src/mpi_communication_tracking_f08_wrappers.f90
+++ src/mpi_communication_tracking_f08_wrappers.f90
@@ -121,3 +121,73 @@ subroutine mpi_wait_f08(request, status, ierror)
   if (present(ierror)) ierror = int(ierr_c)
 end subroutine mpi_wait_f08
 
+subroutine mpi_isend_f08ts(buf, count, datatype, dest, tag, comm, request, ierror)
+  use, intrinsic :: iso_c_binding, only : c_int
+  use mpi_f08, only : MPI_Datatype, MPI_Comm, MPI_Request
+  implicit none
+
+  type(*), dimension(..), asynchronous :: buf
+  integer, intent(in) :: count, dest, tag
+  type(MPI_Datatype), intent(in) :: datatype
+  type(MPI_Comm), intent(in) :: comm
+  type(MPI_Request), intent(out) :: request
+  integer, optional, intent(out) :: ierror
+
+  integer(c_int) :: ierr_c
+  integer(c_int) :: request_f_c
+
+  interface
+    subroutine mpi_trace_f08_isend_helper(buf, count, datatype_f, dest, tag, comm_f, request_f, ierr) bind(C)
+      use, intrinsic :: iso_c_binding, only : c_int
+      type(*), dimension(..), asynchronous :: buf
+      integer(c_int), value :: count, datatype_f, dest, tag, comm_f
+      integer(c_int) :: request_f
+      integer(c_int) :: ierr
+    end subroutine
+  end interface
+
+  call mpi_trace_f08_isend_helper(buf, int(count, c_int), int(datatype%MPI_VAL, c_int), int(dest, c_int), int(tag, c_int), int(comm%MPI_VAL, c_int), request_f_c, ierr_c)
+
+  request%MPI_VAL = request_f_c
+
+  if (present(ierror)) ierror = int(ierr_c)
+end subroutine mpi_isend_f08ts
+
+subroutine mpi_waitall_f08(count, array_of_requests, array_of_statuses, ierror)
+  use, intrinsic :: iso_c_binding, only : c_int
+  use mpi_f08, only : MPI_Request, MPI_Status
+  implicit none
+
+  integer, intent(in) :: count
+  type(MPI_Request), intent(inout) :: array_of_requests(count)
+  type(MPI_Status), intent(out) :: array_of_statuses(count)
+  integer, optional, intent(out) :: ierror
+
+  integer(c_int) :: ierr_c
+  integer(c_int) :: request_f_c(count)
+  integer :: i
+
+  interface
+    subroutine mpi_trace_f08_waitall_helper(count, request_f, statuses, ierr) bind(C)
+      use, intrinsic :: iso_c_binding, only : c_int
+      use mpi_f08, only : MPI_Status
+      integer(c_int), value :: count
+      integer(c_int) :: request_f(*)
+      type(MPI_Status) :: statuses(*)
+      integer(c_int) :: ierr
+    end subroutine
+  end interface
+
+  do i = 1, count
+     request_f_c(i) = array_of_requests(i)%MPI_VAL
+  end do
+
+  call mpi_trace_f08_waitall_helper(int(count, c_int), request_f_c, array_of_statuses, ierr_c)
+
+  do i = 1, count
+     array_of_requests(i)%MPI_VAL = request_f_c(i)
+  end do
+
+  if (present(ierror)) ierror = int(ierr_c)
+end subroutine mpi_waitall_f08
+
